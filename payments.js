7fa6b013e0901b665d6251aa20088e372b716fff
Ozgenur Catal <117957198+ozgenur00@users.noreply.github.com>
2023-10-18 12:14:57 -0400

    all added

--- payments.js
+++ payments.js
@@ -75,4 +75,242 @@ function updateSummary() {
   summaryTds[1].innerHTML = '$' + sumPaymentTotal('tipAmt');
   summaryTds[2].innerHTML =  Math.round(tipPercentAvg) + '%';
 }
+//-----------------------------PAYMENTS TEST
+describe("Payments test (with setup and tear-down)", function() {
+    beforeEach(function () {
+      billAmtInput.value = 100;
+      tipAmtInput.value = 20;
+    });
+  
+    it('should add a new payment to allPayments on submitPaymentInfo()', function () {
+      submitPaymentInfo();
+  
+      expect(Object.keys(allPayments).length).toEqual(1);
+      expect(allPayments['payment1'].billAmt).toEqual('100');
+      expect(allPayments['payment1'].tipAmt).toEqual('20');
+      expect(allPayments['payment1'].tipPercent).toEqual(20);
+    });
+  
+    it('should not add a new payment on submitPaymentInfo() with empty input', function () {
+      billAmtInput.value = '';
+      submitPaymentInfo();
+  
+      expect(Object.keys(allPayments).length).toEqual(0);
+    });
+  
+  
+    it('should create a new payment on createCurPayment()', function () {
+      let expectedPayment = {
+        billAmt: '100',
+        tipAmt: '20',
+        tipPercent: 20,
+      }
+  
+      expect(createCurPayment()).toEqual(expectedPayment);
+    });
+  
+    it('should not create payment with empty input on createCurPayment()', function () {
+      billAmtInput.value = '';
+      tipAmtInput.value = '';
+      let curPayment = createCurPayment();
+  
+      expect(curPayment).toEqual(undefined);
+    });
+  
+    afterEach(function() {
+      billAmtInput.value = '';
+      tipAmtInput.value = '';
+      paymentTbody.innerHTML = '';
+      summaryTds[0].innerHTML = '';
+      summaryTds[1].innerHTML = '';
+      summaryTds[2].innerHTML = '';
+      serverTbody.innerHTML = '';
+      paymentId = 0;
+      allPayments = {};
+    });
+  });
+//-----------------------------HELPERS JS
+
+// accepts 'tipAmt', 'billAmt', 'tipPercent' and sums total from allPayments objects
+function sumPaymentTotal(type) {
+  let total = 0;
+
+  for (let key in allPayments) {
+    let payment = allPayments[key];
+
+    total += Number(payment[type]);
+  }
+
+  return total;
+}
+
+// converts the bill and tip amount into a tip percent
+function calculateTipPercent(billAmt, tipAmt) {
+  return Math.round(100 / (billAmt / tipAmt));
+}
+
+// expects a table row element, appends a newly created td element from the value
+function appendTd(tr, value) {
+  let newTd = document.createElement('td');
+  newTd.innerText = value;
+
+  tr.append(newTd);
+}
+
+//-------------------------HELPERS TEST
+describe("Utilities test (with setup and tear-down)", function() {
+  beforeEach(function () {
+    billAmtInput.value = 100;
+    tipAmtInput.value = 20;
+    submitPaymentInfo();
+  });
+
+  it('should sum total tip amount of all payments on sumPaymentTotal()', function () {
+    expect(sumPaymentTotal('tipAmt')).toEqual(20);
+
+    billAmtInput.value = 200;
+    tipAmtInput.value = 40;
+
+    submitPaymentInfo();
+
+    expect(sumPaymentTotal('tipAmt')).toEqual(60);
+  });
+
+  it('should sum total bill amount of all payments on sumPaymentTotal()', function () {
+    expect(sumPaymentTotal('billAmt')).toEqual(100);
+
+    billAmtInput.value = 200;
+    tipAmtInput.value = 40;
+
+    submitPaymentInfo();
+
+    expect(sumPaymentTotal('billAmt')).toEqual(300);
+  });
+
+  it('should sum total tip percent on sumPaymentTotal()', function () {
+    expect(sumPaymentTotal('tipPercent')).toEqual(20);
+
+    billAmtInput.value = 100;
+    tipAmtInput.value = 20;
+
+    submitPaymentInfo();
+
+    expect(sumPaymentTotal('tipPercent')).toEqual(40);
+  });
+
+  it('should sum tip percent of a single tip on calculateTipPercent()', function () {
+    expect(calculateTipPercent(100, 23)).toEqual(23);
+    expect(calculateTipPercent(111, 11)).toEqual(10);
+  });
+
+  it('should generate new td from value and append to tr on appendTd(tr, value)', function () {
+    let newTr = document.createElement('tr');
+
+    appendTd(newTr, 'test');
+
+    expect(newTr.children.length).toEqual(1);
+    expect(newTr.firstChild.innerHTML).toEqual('test');
+  });
+
+
+  afterEach(function() {
+    billAmtInput.value = '';
+    tipAmtInput.value = '';
+    paymentTbody.innerHTML = '';
+    summaryTds[0].innerHTML = '';
+    summaryTds[1].innerHTML = '';
+    summaryTds[2].innerHTML = '';
+    serverTbody.innerHTML = '';
+    allPayments = {};
+    paymentId = 0;
+  });
+});
+
+//---------------------------SERVERS JS
+let serverNameInput = document.getElementById('serverName');
+let serverForm = document.getElementById('serverForm');
+
+let serverTbody = document.querySelector('#serverTable tbody');
+
+let allServers = {};
+let serverId = 0;
+
+serverForm.addEventListener('submit', submitServerInfo);
+
+// create server object and add to allServers, update html and reset input
+function submitServerInfo(evt) {
+  if (evt) evt.preventDefault(); // when running tests there is no event
+
+  let serverName = serverNameInput.value;
+
+  if (serverName !== '') {
+    serverId++;
+    allServers['server' + serverId] = { serverName };
+
+    updateServerTable();
+
+    serverNameInput.value = '';
+  }
+}
+
+// Create table row element and pass to appendTd function with input value
+function updateServerTable() {
+  serverTbody.innerHTML = '';
+
+  for (let key in allServers) {
+    let curServer = allServers[key];
+
+    let newTr = document.createElement('tr');
+    newTr.setAttribute('id', key);
+
+    let tipAverage = sumPaymentTotal('tipAmt') / Object.keys(allServers).length;
+
+    appendTd(newTr, curServer.serverName);
+    appendTd(newTr, '$' + tipAverage.toFixed(2));
+
+    serverTbody.append(newTr);
+  }
+}
+
+//-----------------------SERVERS TEST
+describe("Servers test (with setup and tear-down)", function() {
+  beforeEach(function () {
+    // initialization logic
+    serverNameInput.value = 'Alice';
+  });
+
+  it('should add a new server to allServers on submitServerInfo()', function () {
+    submitServerInfo();
+
+    expect(Object.keys(allServers).length).toEqual(1);
+    expect(allServers['server' + serverId].serverName).toEqual('Alice');
+  });
+
+  it('should update the server table on submitServerInfo()', function () {
+    submitServerInfo();
+
+    let tableRows = serverTbody.querySelectorAll('tr');
+    expect(tableRows.length).toEqual(1);
+
+    let firstRow = tableRows[0];
+    expect(firstRow.getAttribute('id')).toEqual('server1');
+    expect(firstRow.cells[0].textContent).toEqual('Alice');
+  });
+
+  
+  it('should update the server table when allServers is empty', function () {
+    allServers = {};
+    updateServerTable();
+
+    let tableRows = serverTbody.querySelectorAll('tr');
+    expect(tableRows.length).toEqual(0);
+  });
+
+  afterEach(function() {
+    // teardown logic
+    serverId = 0;
+    serverTbody.innerHtml = '';
+    allServers = {};
+  });
+});
 
